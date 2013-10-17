4468a26d0fa86e5dcc70be00df64288d5f6dc3da
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2013-10-17 08:59:02 +0200

    waf: finish up cpu detection

--- waftools/cpudetect.py
+++ waftools/cpudetect.py
@@ -20,8 +20,11 @@ def configure(ctx):
     ctx.define('ARCH_X86_32', 0)
     ctx.define('ARCH_X86_64', 0)
     ctx.define('HAVE_FAST_64BIT', 0)
-    {
-        'x86_32': x86_32,
-        'x86_64': x86_64,
-        'ia':     ia64
-    }.get(ctx.env.DEST_CPU, default)(ctx)
+
+    ctx.define('HAVE_MMX',   'ARCH_X86', quote=False)
+    ctx.define('HAVE_MMX2',  'ARCH_X86', quote=False)
+    ctx.define('HAVE_SSE',   'ARCH_X86', quote=False)
+    ctx.define('HAVE_SSE2',  'ARCH_X86', quote=False)
+    ctx.define('HAVE_SSSE3', 'ARCH_X86', quote=False)
+
+    locals().get(ctx.env.DEST_CPU, default)(ctx)
